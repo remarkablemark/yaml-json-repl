7ad41f1bbd0dc94823d746e72b0419ac3c94b198
Mark <mark@remarkablemark.org>
2018-05-22 00:16:13 -0400

    feat(public): replace `index.html` head and body with project info

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 document.getElementById('app').innerHTML = `
-  <h1>Welcome to <code>web-app-template</code>.</h1>
-  <a href="https://github.com/remarkablemark/web-app-template">
+  <h1>Welcome to <code>yaml-json-repl</code>.</h1>
+  <a href="https://github.com/remarkablemark/yaml-json-repl">
     View source
   </a>
 `;
