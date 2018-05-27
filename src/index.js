1a42935f4a8aa027bf4816ed76790a656515553e
Mark <mark@remarkablemark.org>
2018-05-26 20:39:41 -0400

    docs(src): add JSDoc comments in `index.js`

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 const { CodeMirror, jsyaml } = window;
 
+/**
+ * Shared editor options.
+ *
+ * @const {Object}
+ */
 const editorOptions = {
   inputStyle: 'contenteditable',
   lineNumbers: true,
@@ -11,23 +16,50 @@ const editorOptions = {
   theme: 'default',
 };
 
+/**
+ * YAML CodeMirror editor on the left (editable).
+ *
+ * @const {Object}
+ */
 const editor1 = CodeMirror.fromTextArea(document.getElementById('editor1'), {
   ...editorOptions,
   autofocus: true,
   mode: 'yaml',
 });
 
+/**
+ * JSON CodeMirror editor on the left (readonly).
+ *
+ * @const {Object}
+ */
 const editor2 = CodeMirror.fromTextArea(document.getElementById('editor2'), {
   ...editorOptions,
   mode: 'javascript',
   readOnly: true,
 });
 
-// indent with spaces instead of tabs
-// https://gist.github.com/danieleds/326903084a196055a7c3
+/**
+ * Add extra keys to YAML editor.
+ *
+ * @const {Object}
+ */
 editor1.setOption('extraKeys', {
-  'Shift-Tab': cm => cm.indentSelection('subtract'),
+  /**
+   * Subtracts 2 spaces.
+   *
+   * @see {@link https://gist.github.com/danieleds/326903084a196055a7c3 Gist}
+   * @param {CodeMirror} cm
+   */
+  'Shift-Tab': cm => {
+    cm.indentSelection('subtract');
+  },
 
+  /**
+   * Indents with 2 spaces instead of tab.
+   *
+   * @see {@link https://gist.github.com/danieleds/326903084a196055a7c3 Gist}
+   * @param {CodeMirror} cm
+   */
   Tab: cm => {
     const selection = cm.somethingSelected() ? editor1.getSelection('\n') : '';
     if (
@@ -41,6 +73,9 @@ editor1.setOption('extraKeys', {
   },
 });
 
+/**
+ * Converts YAML to JSON on editor change and displays errors if applicable.
+ */
 editor1.on('change', cm => {
   const value = cm.getValue();
   if (!value.trim()) {
